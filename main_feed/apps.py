bb585bf13d230681382a0be33b96bc604dfcc2e5
dim-gggl <dimitri.gaggioli@gmail.com>
2025-06-05 20:34:52 +0200

    Amélioration de l'accessibilité avec des attributs tabindex

--- main_feed/apps.py
+++ main_feed/apps.py
@@ -1,6 +1,11 @@
 from django.apps import AppConfig
 
 
+"""
+The Class MainFeedConfig permits to configure the way primary keys
+are automatically given to instances of our models if not explicitly
+implemented inside the constructor
+"""
 class MainFeedConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'main_feed'
